08ff375309de4dd4a6ca77212217c9b04bc1687b
Aredruss <aredruss@gmail.com>
2022-04-16 11:42:48 +0300

    Created the script and a basic text file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+import java.io.File
+
+fun main() {
+
+    val adverb = "[а-я]*[нкр][о][ ]".toRegex()
+    val adjective = "[а-я]*[аиюы][й][ ]".toRegex()
+    val verb = "[а-я]*[ть][ ]".toRegex()
+
+    val contents = readFileAsTextUsingInputStream("/Users/areduss/Projects/Reader/text.txt")
+
+    println(contents)
+    println("Наречия: ${adverb.findAll(contents).count()}")
+    println("Прилагательные: ${adjective.findAll(contents).count()}")
+       println("Глаголы: ${verb.findAll(contents).count()}")
+
+}
+
+
+fun readFileAsTextUsingInputStream(fileName: String) = File(fileName).inputStream().readBytes().toString(Charsets.UTF_8)
